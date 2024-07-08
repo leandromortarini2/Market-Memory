e7997e5c50d3a49236bd581f6df912acf08f7f16
leandro mortarini <leandromortarini2@gmail.com>
2024-07-08 16:47:31 -0300

    conexion con base de datos PostgreSQL / TypeORM

--- Back-market-memory/src/index.ts
+++ Back-market-memory/src/index.ts
@@ -1,6 +1,12 @@
 import server from "./server";
 import { PORT } from "./config/env";
+import "reflect-metadata";
+import { AppDataSource } from "./config/data-source";
+import { log } from "console";
 
-server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+AppDataSource.initialize().then((res) => {
+  console.log("conexion con base de datos exitosa");
+  server.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+  });
 });
