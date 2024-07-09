16e2adf748d371092b9b46c5a4d1370934918f53
leandro mortarini <leandromortarini2@gmail.com>
2024-07-09 17:30:43 -0300

    creacion de tablas con TypeORM

--- Back-market-memory/src/index.ts
+++ Back-market-memory/src/index.ts
@@ -2,7 +2,6 @@ import server from "./server";
 import { PORT } from "./config/env";
 import "reflect-metadata";
 import { AppDataSource } from "./config/data-source";
-import { log } from "console";
 
 AppDataSource.initialize().then((res) => {
   console.log("conexion con base de datos exitosa");
