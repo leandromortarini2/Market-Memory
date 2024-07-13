aa526daf1bb8a31133e00d355f29d7e798d3e17f
leandro mortarini <leandromortarini2@gmail.com>
2024-07-13 17:17:59 -0300

    navbar responsive y home estilos y responsive

--- Front-market-memory/src/components/NavBar/NavBar.tsx
+++ Front-market-memory/src/components/NavBar/NavBar.tsx
@@ -1,35 +1,88 @@
+"use client";
 import React from "react";
 import Image from "next/image";
-import Logo from "../../assent/logoblanco.png";
+import Logo from "../../assent/cart.svg";
+import LogoBurger from "../../assent/burger.svg";
 import Link from "next/link";
+import { useState } from "react";
 
 export const NavBar = () => {
+  const [burger, setBurger] = useState(false);
+
+  const handleBurger = () => {
+    setBurger(!burger);
+  };
+
   return (
-    <div className="w-full h-14 flex justify-between  items-center  fixed containerMain">
-      <div className="w-1/2 h-24 flex justify-evenly items-center">
-        <Link href="#sec1">
-          <button className="text-xl text-white font-semibold">Home</button>
-        </Link>
-        <a href="#sec2">
-          <button className="text-xl text-white font-semibold ">
-            Add Product
-          </button>
-        </a>
-        <Link href="#sec3">
-          <button className="text-xl text-white font-semibold ">
-            shopping list
-          </button>
-        </Link>
+    <>
+      <div className="hidden w-full h-14 lg:flex justify-between  items-center bg-white  fixed shadow-gray-300 shadow-md z-50 ">
+        <div className="w-1/2 h-24 flex justify-evenly items-center  ">
+          <Link href="#sec1">
+            <button className="text-2xl text-gray-700 font-bold hover:text-green-500 ">
+              Home
+            </button>
+          </Link>
+          <a href="#sec2">
+            <button className="text-2xl text-gray-700 font-bold hover:text-green-500 ">
+              Add Product
+            </button>
+          </a>
+          <Link href="#sec3">
+            <button className="text-2xl text-gray-700 font-bold hover:text-green-500 ">
+              shopping list
+            </button>
+          </Link>
+        </div>
+        <div className="w-1/4 flex justify-center">
+          <Link href="#sec3">
+            <button className="text-2xl text-gray-700 font-bold hover:text-green-500 ">
+              LogIn{" "}
+            </button>
+          </Link>
+        </div>
       </div>
-      <div className="w-1/4 h-14 flex items-center justify-center  ">
-        <Link href="#sec3">
-          <button className="text-xl text-white font-semibold ">LogIn</button>
-        </Link>
+      <div className="w-full h-14 bg-white ShadowEffect2 flex lg:hidden fixed z-50">
+        <div className="w-1/2  flex justify-center items-center">
+          <h1 className=" text-xl   text-gray-800 font-bold  ">
+            Market
+            <span className="text-green-500">|Memory</span>
+          </h1>{" "}
+          <Image src={Logo} alt="" className="w-10" />
+        </div>
+        <div className="w-1/2  flex justify-end items-center">
+          <button onClick={handleBurger}>
+            <Image src={LogoBurger} alt="" className="w-10" />
+          </button>
+        </div>
       </div>
-    </div>
+
+      {burger === true ? (
+        <div className="w-full h-72 z-30 flex flex-col justify-evenly items-end fixed md:hidden ">
+          <div className="w-1/2 h-52 flex flex-col justify-evenly items-end p-10 bg-gray-100 ShadowEffect2">
+            <Link href="#sec1">
+              <button className="text-xl text-gray-800  font-semibold hover:text-green-600">
+                Home
+              </button>
+            </Link>
+            <a href="#sec2">
+              <button className="text-xl text-gray-800  font-semibold hover:text-green-600">
+                Add Product
+              </button>
+            </a>
+            <Link href="#sec3">
+              <button className="text-xl text-gray-800  font-semibold hover:text-green-600">
+                shopping list
+              </button>
+            </Link>
+
+            <Link href="#sec3">
+              <button className="text-xl text-gray-800  font-semibold hover:text-green-600">
+                LogIn{" "}
+              </button>
+            </Link>
+          </div>
+        </div>
+      ) : null}
+    </>
   );
 };
-
-// <div className="w-1/4 h-20 flex items-center justify-center">
-//   <Image src={Logo} alt="" className="w-60" />
-// </div>;
--- Front-market-memory/src/components/Portada/Portada.tsx
+++ Front-market-memory/src/components/Portada/Portada.tsx
@@ -1,30 +1,78 @@
 import React from "react";
 import Logo from "../../assent/cart.svg";
 import Image from "next/image";
+import Link from "next/link";
 
 export const Portada = () => {
   return (
-    <div id="sec1" className="w-full min-h-screen  flex  items-end imgPortada">
-      <div className="w-1/2 h-72  flex flex-col justify-evenly ml-20 mb-24">
-        <h2 className="text-4xl text-white font-bold ShadowEffect2 capitalize">
-          welcome
-        </h2>
-        <div className="w-full flex">
-          {" "}
-          <h1 className="text-[100px] text-white font-bold ShadowEffect2">
-            Market<span className="text-green-500">|Memory</span>
-          </h1>{" "}
-          <Image src={Logo} alt="" className="w-32" />
-        </div>
+    <>
+      {/* pantalla MD en adelante */}
+      <div
+        id="sec1"
+        className="hidden w-full min-h-screen  md:flex flex-col items-center justify-center md:justify-start md:flex-row  md:items-center  "
+      >
+        <div className=" w-full md:w-1/3 h-72  md:flex flex-col justify-evenly items-center md:items-start md:ml-20  ">
+          <h2 className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl text-gray-500 font-bold  capitalize">
+            welcome
+          </h2>
+          <div className="w-full flex justify-center md:justify-start ">
+            {" "}
+            <h1 className=" text-3xl md:text-6xl lg:text-[80px] xl:text-[100px] text-gray-800 font-bold  ">
+              Market
+              <span className="text-green-500">|Memory</span>
+            </h1>{" "}
+            <Image src={Logo} alt="" className="w-14 md:w-20 lg:w-28 xl:w-32" />
+          </div>
 
-        <div className="w-3/4  ">
-          {" "}
-          <p className="text-gray-100 text-3xl font-medium capitalize ShadowEffect2">
-            Never forget anything again, everything in its place, always within
-            your reach.{" "}
-          </p>
+          <div className=" ">
+            <p className="text-gray-600 text-lg md:text-xl lg:text-2xl xl:text-3xl font-medium capitalize  text-center md:text-start">
+              Never forget anything again, everything in its place, always
+              within your reach.
+            </p>
+          </div>
+          <div className="w-full  flex justify-center md:justify-start mt-5">
+            <Link href="#sec3">
+              <button className="w-32 xl:w-40 bg-green-500 p-2 rounded-xl text-gray-100 text-lg font-bold md:text-xl lg:text-2xl xl:text-3xl  capitalize  hover:bg-green-700 ">
+                my list
+              </button>
+            </Link>
+          </div>
+        </div>
+        <div className=" w-3/4 min-h-screen imgPortada imgEffectLeft"></div>
+      </div>
+      {/* pantalla Movil */}
+      <div
+        id="sec1"
+        className="w-full min-h-screen imgPortada2 flex flex-col justify-center items-center md:hidden  "
+      >
+        <div className=" w-full h-80  flex flex-col justify-evenly items-center  ">
+          <h2 className="text-3xl  text-gray-500 font-bold  capitalize">
+            welcome
+          </h2>
+          <div className="w-full flex justify-center md:justify-start ">
+            {" "}
+            <h1 className=" text-4xl  text-gray-800 font-bold  ">
+              Market
+              <span className="text-green-500">|Memory</span>
+            </h1>{" "}
+            <Image src={Logo} alt="" className="w-14 md:w-20 lg:w-28 xl:w-32" />
+          </div>
+
+          <div className=" ">
+            <p className="text-gray-600 text-xl  font-semibold capitalize  text-center ">
+              Never forget anything again, everything in its place, always
+              within your reach.
+            </p>
+          </div>
+          <div className="w-full  flex justify-center md:justify-start mt-5">
+            <Link href="#sec3">
+              <button className="w-32 xl:w-40 bg-green-500 p-2 rounded-xl text-gray-100 text-lg font-bold md:text-xl lg:text-2xl xl:text-3xl  capitalize  hover:bg-green-700 ">
+                my list
+              </button>
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
