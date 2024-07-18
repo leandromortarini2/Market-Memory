d06711a925f4c5833c97ee83bb9647f5ab2a8e43
leandro mortarini <leandromortarini2@gmail.com>
2024-07-18 16:16:36 -0300

    estilos form

--- Front-market-memory/src/components/form/Form.tsx
+++ Front-market-memory/src/components/form/Form.tsx
@@ -3,41 +3,27 @@ import { addProduct } from "@/helpers/postProduct";
 import { IProduct } from "@/Interface/IProduct";
 import React, { useEffect } from "react";
 import { useState } from "react";
+import { ListProducts } from "../ListProducts/ListProducts";
 
 const Form: React.FC = () => {
-  const [quantity, setQuantity] = useState(1);
   const [product, setProduct] = useState<IProduct>({
     name: "",
     type: "A" || "B" || "C" || "D",
-    quantity: 1,
+    quantity: 0,
   });
 
-  useEffect(() => {
-    // Actualiza el estado del producto cuando quantity cambia
-    setProduct((prevProduct) => ({
-      ...prevProduct,
-      quantity: quantity, // Actualiza quantity en lugar de count
-    }));
-  }, [quantity]);
-  const handleCountSuma = (event: React.MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault();
-
-    setQuantity(quantity + 1);
-  };
-  const handleCountResta = (event: React.MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault();
-
-    setQuantity(quantity - 1);
-  };
+  // Actualiza el estado del producto cuando quantity cambia
 
   const handleOnChange = (
     event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
   ) => {
     const { name, value } = event.target;
-    setProduct((prevProduct) => ({
-      ...prevProduct,
+    setProduct({
+      ...product,
       [name]: value,
-    }));
+    });
+
+    console.log("Producto agregado:", product);
   };
 
   const handleOnSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
@@ -53,78 +39,56 @@ const Form: React.FC = () => {
   };
 
   return (
-    <div className="w-full min-h-screen flex flex-col md:flex-row justify-center items-center  ">
-      <div className="w-full md:w-1/4 md:h-screen flex flex-col justify-center items-center ShadowEffect2 bg-white ">
-        <h2
-          id="sec2"
-          className="text-xl xl:text-3xl text-green-700 font-semibold capitalize mt-5 "
+    <div className="w-full flex flex-col justify-center items-center bg-white ShadowEffect2">
+      <form
+        onSubmit={handleOnSubmit}
+        action=""
+        className="w-full h-20 md:w-3/4 lg:h-32  flex flex-wrap  md:flex-nowrap  justify-evenly items-center pb-2 "
+      >
+        <input
+          type="text"
+          name="name"
+          value={product.name}
+          onChange={handleOnChange}
+          placeholder="Producto..."
+          className="w-1/2 md:w-1/3  md:h-8 lg:h-10  md:placeholder:text-sm lg:placeholder:text-lg  placeholder:text-green-900 font-semibold pl-2 text-md md:text-lg bg-transparent border-b-4 border-green-900 "
+        />
+        <select
+          name="quantity"
+          id=""
+          value={product.quantity}
+          onChange={handleOnChange}
+          className="w-1/6  md:h-8 lg:h-10  md:placeholder:text-sm lg:placeholder:text-lg  md:text-sm lg:text-xl text-green-900 text-center font-semibold bg-transparent border-b-4 border-green-900 "
         >
-          Add new Product
-        </h2>
-        <form
-          onSubmit={handleOnSubmit}
-          action=""
-          className="w-full min-h-[300px]  flex flex-col justify-evenly items-center"
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+        </select>
+        {/* SELECT */}
+        <select
+          name="type"
+          id=""
+          value={product.type}
+          onChange={handleOnChange}
+          className="w-1/2 md:w-1/4 lg:w-1/4 md:h-8 lg:h-10 placeholder:text-sm lg:placeholder:text-lg  placeholder:pl-2 text-green-900 text-md md:text-lg font-semibold bg-transparent border-b-4 border-green-900"
         >
-          <input
-            type="text"
-            name="name"
-            value={product.name}
-            onChange={handleOnChange}
-            placeholder="Nombre del producto"
-            className="w-3/4 border-b-2 border-green-700 md:placeholder:text-sm lg:placeholder:text-lg "
-          />
+          <option value="A">A-Cocina</option>
+          <option value="B">B-Limpieza</option>
+          <option value="C">C-Caniceria/Polleria</option>
+          <option value="D">D-Fruta/Verdura</option>
+        </select>
+        {/* INPUT */}
+        {/* INPUT */}
 
-          {/* SELECT */}
-          <select
-            name="type"
-            id=""
-            value={product.type}
-            onChange={handleOnChange}
-            className="w-3/4 border-b-2 border-green-700 md:placeholder:text-sm lg:placeholder:text-lg  md:text-sm lg:text-lg"
-          >
-            <option value="">Seccion del Articulo</option>
-            <option value="A">A-Cocina</option>
-            <option value="B">B-Limpieza</option>
-            <option value="C">C-Caniceria/Polleria</option>
-            <option value="D">D-Fruta/Verdura</option>
-          </select>
-          {/* INPUT */}
-          {/* INPUT */}
-          <div className="w-full  flex flex-col justify-center items-center">
-            <input
-              type="text"
-              name="count"
-              placeholder=""
-              value={quantity}
-              readOnly
-              className="w-1/2 text-center text-lg md:text-xl lg:text-3xl font-bold "
-            />
-            <div className="w-1/2">
-              {" "}
-              <button
-                onClick={handleCountSuma}
-                className="w-1/2 text-green-900  text-6xl border-none ShadowEffect2"
-              >
-                +
-              </button>
-              <button
-                onClick={handleCountResta}
-                className="w-1/2 h-10 text-red-900  text-6xl ShadowEffect2"
-              >
-                -
-              </button>
-            </div>
-          </div>
-          <button
-            type="submit"
-            className=" lg:w-3/4 lg:h-10 bg-green-800 text-white md:text-md xl:text-xl rounded-lg p-2"
-          >
-            Add Product
-          </button>
-        </form>
-      </div>
-      <div className="w-full md:w-3/4 h-screen bg-green-100"></div>
+        <button
+          type="submit"
+          className="w-1/6 md:h-8 lg:h-10  bg-green-900 text-white font-extrabold  md:text-xl xl:text-xl rounded-lg hover:bg-green-500 duration-1000 "
+        >
+          +
+        </button>
+      </form>
     </div>
   );
 };
